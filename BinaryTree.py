9436f6e0b7344598de229124a44bb614a4752752
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2023-01-15 09:29:57 +0800

    def delete

--- BinaryTree.py
+++ BinaryTree.py
@@ -73,6 +73,28 @@ class BinarySearchTreeNode:
 
         return elements
 
+    def delete(self,val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+
+            minimum_val = self.right.minimum()
+            self.data = minimum_val
+            self.right = self.right
+
+        return self
+
 
 
 
