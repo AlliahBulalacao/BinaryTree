0385fe700ce054b9f71a205f3b2b41fe1c91581d
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2023-01-15 08:30:46 +0800

    def in order

--- BinaryTree.py
+++ BinaryTree.py
@@ -37,6 +37,18 @@ class BinarySearchTreeNode:
             else:
                 return False
 
-    def
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+            elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+
 
 
